9c9152a1d64830404ee9522521d0cd78b3e420f6
ottyanna <ottyanna@gmail.com>
2022-03-11 16:02:40 +0100

    Added newHDRImage procedure

--- src/basictypes.nim
+++ src/basictypes.nim
@@ -5,3 +5,10 @@ type
     HdrImage* = object
         width*, height*: int
         pixels*: seq[Color]
+
+#this function creates a new black image (the Color fields are created 0 by default)
+proc newHDRImage* ( width, height : int ) : HdrImage =
+
+    result.width = width
+    result.height = height
+    result.pixels = newSeq[Color] (width*height)
