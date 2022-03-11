45236fc23db59818446710079aed4d7aa0816abf
ottyanna <ottyanna@gmail.com>
2022-03-11 17:39:09 +0100

    Updated .gitignore

--- tests/test_basictypes.nim
+++ tests/test_basictypes.nim
@@ -3,4 +3,7 @@ import ../src/basictypes
 when isMainModule:
     let col1 = Color(r : 1.0, g: 2.0, b: 3.0)
     let col2 = Color(r : 5.0, g: 7.0, b: 9.0)
-    assert col1 + col2 == Color(r: 4.0, g: 6.0, b: 8.0)
+
+    #assert (col1 + col2).areColorClose(Color(6.0, 9.0, 12.0))
+    #assert (col1 - col2).is_close(Color(-4.0, -5.0, -6.0))
+    #assert (col1 * col2).is_close(Color(5.0, 14.0, 27.0))
