17a06fcbf1d479dcc5ed2fa571ee6420ee64fe37
ottyanna <ottyanna@gmail.com>
2022-03-11 17:29:23 +0100

    Added some tests

--- tests/test_basictypes.nim
+++ tests/test_basictypes.nim
@@ -1,1 +1,6 @@
-#prova
+import ../src/basictypes
+
+when isMainModule:
+    let col1 = Color(r : 1.0, g: 2.0, b: 3.0)
+    let col2 = Color(r : 5.0, g: 7.0, b: 9.0)
+    assert col1 + col2 == Color(r: 4.0, g: 6.0, b: 8.0)
