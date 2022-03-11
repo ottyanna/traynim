3809d69359ac28823767292036a2b34f2a52b04f
ottyanna <ottyanna@gmail.com>
2022-03-11 16:44:00 +0100

    Added test

--- src/traynim.nim
+++ src/traynim.nim
@@ -3,7 +3,7 @@ import basictypes
 when isMainModule:
   echo("Hello, World!")
 
-  #let col1 : Color = (r : 1.0, g: 2.0, b : 3.0)
-  #let col2 = Color(5.0, 7.0, 9.0)
+  let col1 = Color(r : 1.0, g: 2.0, b : 3.0)
+  let col2 = Color(r : 5.0, g: 7.0, b: 9.0)
 
-  #echo(col1 + col2)
+  echo(col1 + col2)
