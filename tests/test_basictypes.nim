8e8bd17e2070f4ff929f376183a2cf0bdf14d59f
Jacopo Fera <jacopof@gmail.com>
2022-03-15 11:44:52 +0100

    Merge test

--- tests/test_basictypes.nim
+++ tests/test_basictypes.nim
@@ -2,26 +2,23 @@ import ../src/basictypes
 
 when isMainModule:
 
-    # Tests on Color operations
-    let col1 = Color(r: 1.0, g: 2.0, b: 3.0)
-    let col2 = Color(r: 5.0, g: 7.0, b: 9.0)
+    #tests on colors operations
+    let col1 = Color(r : 1.0, g: 2.0, b: 3.0)
+    let col2 = Color(r : 5.0, g: 7.0, b: 9.0)
 
     assert (col1 + col2).areColorsClose(Color(r: 6.0, g: 9.0, b: 12.0))
     assert (col1 - col2).areColorsClose(Color(r: -4.0, g: -5.0, b: -6.0))
     assert (col1 * col2).areColorsClose(Color(r: 5.0, g: 14.0, b: 27.0))
     assert not (col1 + col2).areColorsClose(Color(r: 3.0, g: 9.0, b: 12.0))
-    assert (col1*2.0).areColorsClose(Color(r: 2.0, g: 4.0, b: 6.0))
-    assert (3*col2).areColorsClose(Color(r: 15.0, g: 21.0, b: 27.0))
-    assert not (3.0*col2).areColorsClose(Color(r: 1.0, g: 21.0, b: 27.0))
 
-    # Tests on HDRimage operations
-    let img = newHDRImage(7, 4)
+    let img = newHDRImage(7,4)
 
+    #test on HDRimage
     assert img.width == 7
     assert img.height == 4
-    assert not (img.width == 27) # Parenthesis are needed
+    #assert not img.width == 27
 
-    # Check that valid/invalid coordinates are properly ﬂagged
+    #test for coordinates
     assert validCoordinates(img, 0, 0)
     assert validCoordinates(img, 6, 3)
     assert not validCoordinates(img, -1, 0)
@@ -29,7 +26,15 @@ when isMainModule:
     assert not validCoordinates(img, 7, 0)
     assert not validCoordinates(img, 0, 4)
 
-    # Check that indices in the array are calculated correctly
+    #test on pixel offset
     assert pixelOffset(img, 0, 0) == 0
     assert pixelOffset(img, 3, 2) == 17
     assert pixelOffset(img, 6, 3) == 7 * 4 - 1
+
+    #test 
+    def test_get_set_pixel():
+    img = HdrImage(7, 4)
+
+    reference_color = Color(1.0, 2.0, 3.0)
+    img.set_pixel(3, 2, reference_color)
+    assert are_colors_close(reference_color, img.get_pixel(3, 2))
