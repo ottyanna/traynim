b56b8bda67154206413fa54fb428d3bf95047848
Jacopo Fera <jacopof@gmail.com>
2022-03-30 12:40:27 +0200

    Add template  proc

--- src/geometry.nim
+++ src/geometry.nim
@@ -10,3 +10,23 @@ type
 
     Normal* = object
         x*, y*, z* : float64
+
+
+proc newVec*(x, y, z: float64): Vec =
+
+    ## Creates a new color from scratch
+
+    result.x = x
+    result.y = y
+    result.z = z
+
+template print3Dop(type1: typedesc) =
+    proc `$`*(a: type1): string =
+        ## Parse a 3D obj as a string
+        result = "<" & "x: " & $(a.x) & " , " & "y: " & $(a.y) & ", " &
+            "z: " & $(a.z) & ">"
+
+print3Dop(Vec)
+print3Dop(Point)
+print3Dop(Normal)
+    
