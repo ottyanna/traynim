8e4950455f9cdd0d38e5225f0c22724b0a6e4c1d
ottyanna <ottyanna@gmail.com>
2022-03-31 13:24:01 +0200

    Changed defineMirrorOp from neg() to `-`() overload, added 3dObj*scalar procedure, added areClose procedure, added tests for Vec Procedures

--- src/geometry.nim
+++ src/geometry.nim
@@ -1,3 +1,23 @@
+#traynim is a ray tracer program written in Nim
+#Copyright (C) 2022 Jacopo Fera, Anna Spanò
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#encoding: utf-8
+
+import colors
+
 type
     Point* = object
         x*,y*,z* :float64
@@ -55,20 +75,33 @@ template defineDotProd(type1: typedesc, type2: typedesc) =
 defineDotProd(Vec,Vec)    
 defineDotProd(Vec,Normal)
 
-template defineProdWithScalar(rettype: typedesc) =
+template defineProdScalar3dObj(rettype: typedesc) =
     proc `*`*(scalar: float64, a: rettype): rettype = 
 
         ## Implements scalar product with a 3d objects such as `Vec` and `Normal` operation
          
-        result.x = scalar * a.x
+        result.x = scalar * a.x 
         result.y = scalar * a.y
         result.z = scalar * a.z
 
-defineProdWithScalar(Vec)
-defineProdWithScalar(Normal)    
+defineProdScalar3dObj(Vec)
+defineProdScalar3dObj(Normal)    
+
+template defineProd3dObjScalar(rettype: typedesc) =
+    proc `*`*(a: rettype, scalar: float64): rettype = 
+
+        ## Implements scalar product with a 3d objects such as `Vec` and `Normal` operation
+         
+        result.x = scalar * a.x 
+        result.y = scalar * a.y
+        result.z = scalar * a.z
+
+defineProd3dObjScalar(Vec)
+defineProd3dObjScalar(Normal)    
+
 
 template defineMirrorOp(rettype: typedesc) =
-    proc neg*(a: var rettype): rettype = 
+    proc `-`*(a: rettype): rettype = 
         result.x = -a.x
         result.y = -a.y
         result.z = -a.z
@@ -77,13 +110,26 @@ defineMirrorOp(Vec)
 defineMirrorOp(Normal)
     
 
-template print3dObj(fname: untyped, type1: typedesc) =
-    proc fname*(a: type1): string =
+template definePrint3dObj(type1: typedesc) =
+    proc `$`*(a: type1): string =
         ## Parse a 3D obj as a string
         result = "<" & "x: " & $(a.x) & " , " & "y: " & $(a.y) & ", " &
             "z: " & $(a.z) & ">"
 
-print3dObj(`$`, Vec)
-print3dObj(`$`, Point)
-print3dObj(`$`, Normal)
-    
+definePrint3dObj(Vec)
+definePrint3dObj(Point)
+definePrint3dObj(Normal)
+
+template defineAreClose3dObj(type1: typedesc) =
+    proc areClose*(a, b: type1): bool =
+        
+        ##
+        ## Determines if two 3d objects are equal (to use with floating points)
+        ## 
+        
+        return areClose(a.x,b.x) and areClose(a.y,b.y) and
+                areClose(a.z,b.z)
+
+defineAreClose3dObj(Vec)
+defineAreClose3dObj(Point)
+defineAreClose3dObj(Normal)
--- tests/test_geometry.nim
+++ tests/test_geometry.nim
@@ -1,10 +1,51 @@
+#traynim is a ray tracer program written in Nim
+#Copyright (C) 2022 Jacopo Fera, Anna Spanò
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#encoding: utf-8
+
+
 import ../src/geometry
+import ../src/colors
 
-var v : Vec 
-v.new3dOp(1.0, 1.0, 1.0)
 
-echo ($v)
+proc testVecCreation (a :Vec) =
+    assert areClose(a.x,1.0)
+    assert not areClose(a.y,5.0)
+    assert areClose(a.z,3.0)
 
-v = newVec(2.0,3.0,4.0)
 
-echo $v
+proc testVecOperations (a, b :Vec) =
+    assert not (($b) == "<x: 1.0 , y: 2.0, z: 3.0>")
+    assert ($a) == "<x: 1.0 , y: 2.0, z: 3.0>"
+    assert (-a).areClose(newVec(-1.0, -2.0, -3.0))
+    assert (a + b).areClose(newVec(5.0, 8.0, 11.0))
+    assert (b - a).areClose(newVec(3.0, 4.0, 5.0))
+    assert ( 2 * a).areClose(newVec(2.0, 4.0, 6.0))
+    assert ( a * 2).areClose(newVec(2.0, 4.0, 6.0))
+    assert areClose(40.0,a*b)
+    #assert a.cross(b).areClose(newVec(-2.0, 4.0, -2.0))
+    #assert b.cross(a).areClose(newVec(2.0, -4.0, 2.0))
+    #assert pytest.approx(14.0) == a.squared_norm()
+    #assert pytest.approx(14.0) == a.norm() ** 2
+
+
+when isMainModule:
+    
+    var a = newVec(1.0, 2.0, 3.0)
+    var b = newVec(4.0, 6.0, 8.0)
+
+    testVecCreation(a)
+    testVecOperations(a,b)
