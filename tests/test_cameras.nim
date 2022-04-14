09ad2a80b38a30f7f41760183e28edee7f506841
Jacopo Fera <jacopof@gmail.com>
2022-04-14 19:54:34 +0200

    [skip ci] Add testOrthogonalCameraTransform

--- tests/test_cameras.nim
+++ tests/test_cameras.nim
@@ -23,14 +23,14 @@ import ../src/traynim/geometry
 import ../src/traynim/ray
 import ../src/traynim/transformations
 
-proc testOrthogonalCamera()=
-    
+proc testOrthogonalCamera() =
+
     let cam = newOrthogonalCamera(aspectRatio = 2.0)
-    let ray1 = cam.fireRay(0.0,0.0)
-    let ray2 = cam.fireRay(1.0,0.0)
-    let ray3 = cam.fireRay(0.0,1.0)
-    let ray4 = cam.fireRay(1.0,1.0)
-    
+    let ray1 = cam.fireRay(0.0, 0.0)
+    let ray2 = cam.fireRay(1.0, 0.0)
+    let ray3 = cam.fireRay(0.0, 1.0)
+    let ray4 = cam.fireRay(1.0, 1.0)
+
     # Verify that the rays are parallel by verifying that cross-products vanish
     assert areClose(0.0, ray1.dir.cross(ray2.dir).sqrNorm())
     assert areClose(0.0, ray1.dir.cross(ray3.dir).sqrNorm())
@@ -38,10 +38,10 @@ proc testOrthogonalCamera()=
 
     # Verify that the ray hitting the corners have the right coordinates
 
-    assert ray1.at(1.0).areClose(newPoint(0.0,2.0,-1.0))
-    assert ray2.at(1.0).areClose(newPoint(0.0,-2.0,-1.0))
-    assert ray3.at(1.0).areClose(newPoint(0.0,2.0,1.0))
-    assert ray4.at(1.0).areClose(newPoint(0.0,-2.0,1.0))
+    assert ray1.at(1.0).areClose(newPoint(0.0, 2.0, -1.0))
+    assert ray2.at(1.0).areClose(newPoint(0.0, -2.0, -1.0))
+    assert ray3.at(1.0).areClose(newPoint(0.0, 2.0, 1.0))
+    assert ray4.at(1.0).areClose(newPoint(0.0, -2.0, 1.0))
 
 proc testIsClose() =
     let ray1 = newRay(origin = newPoint(1.0, 2.0, 3.0), dir = newVec(5.0, 4.0, -1.0))
@@ -61,12 +61,21 @@ proc testTranform() =
     let ray = newRay(newPoint(1.0, 2.0, 3.0), newVec(6.0, 5.0, 4.0))
     let transformation = translation(newVec(10.0, 11.0, 12.0)) * rotationX(90.0)
     let transformed = ray.transform(transformation)
-    
+
     assert transformed.origin.areclose(newPoint(11.0, 8.0, 14.0))
     assert transformed.dir.areclose(newVec(6.0, -4.0, 5.0))
-    
 
-    
+proc testOrthogonalCameraTransform() =
+
+    let cam = newOrthogonalCamera(transformation = translation(-2.0 * newVec(
+            0.0, 1.0, 0.0)) * rotationY(theta = 90))
+    let ray = cam.fireRay(0.5, 0.5)
+
+    assert ray.at(1.0).areClose(newPoint(0.0, -2.0, 0.0))
+
+
+
+
 
 
 when isMainModule:
@@ -74,3 +83,4 @@ when isMainModule:
     testAt()
     testTranform()
     testOrthogonalCamera()
+    testOrthogonalCameraTransform()
