309ce816a0f314f60707c2d848d62c690241ad6f
ottyanna <ottyanna@gmail.com>
2022-04-24 13:15:33 +0200

    Added top-level comments

--- src/traynim/cameras.nim
+++ src/traynim/cameras.nim
@@ -23,44 +23,47 @@ import transformations, ray, geometry
 
 
 type
-    Camera* = ref object of RootObj 
+    Camera* = ref object of RootObj
 
         ## An abstract object representing an observer.
         ## Concrete types are `OrthogographicCamera` and `PerspectiveCamera`.
- 
+
         aspectRatio*: float64
         transformation*: Transformation
 
 type
     OrthogonalCamera* = ref object of Camera
+
         ## A camera implementing an orthogonal 3D → 2D projection.
         ## It implements an observer seeing the world through an orthogonal projection.
 
 type
     PerspectiveCamera* = ref object of Camera
+
         ## A camera implementing a perspective 3D → 2D projection.
         ## It implements an observer seeing the world through a perspective projection.
+
         screenDistance*: float64
 
 method fireRay*(c: Camera, u: float64, v: float64): Ray {.base.} =
-    ## base method, to be overrode by concrete `Camera` derived types methods.
+
+    ## Base method, to be overrode by concrete `Camera` derived types methods.
+
     quit "to override!"
 
 proc newPerspectiveCamera*(aspectRatio = 1.0, screenDistance = 1.0,
         transformation = newTransformation()): PerspectiveCamera =
 
     ## Creates a new perspective camera.
-    ## 
-    ## The parameter `aspectRatio` defines the ratio between the width and the height of the image. 
+    ##
+    ## The parameter `aspectRatio` defines the ratio between the width and the height of the image.
     ## For fullscreen images, set `aspectRatio` to 16/9.
-    ## 
+    ##
     ## The parameter `screenDistance` tells how much far from the eye of the observer is the screen,
     ## and it influences the so-called «aperture» (the field-of-view angle along the horizontal direction).
     ##
-    ## The `transformation` parameter is used to move the camera around and 
+    ## The `transformation` parameter is used to move the camera around and
     ## refers to an object of `Transformation` type.
-
-
 
     new(result)
     result.aspectRatio = aspectRatio
@@ -70,11 +73,11 @@ proc newPerspectiveCamera*(aspectRatio = 1.0, screenDistance = 1.0,
 proc newOrthogonalCamera*(aspectRatio = 1.0, transformation = newTransformation()): OrthogonalCamera =
 
     ## Creates a new orthographic camera.
-    ## 
-    ## The parameter `aspectRatio` defines the ratio between the width and the height of the image. 
+    ##
+    ## The parameter `aspectRatio` defines the ratio between the width and the height of the image.
     ## For fullscreen images, set `aspectRatio` to 16/9.
-    ## 
-    ## The `transformation` parameter is used to move the camera around and 
+    ##
+    ## The `transformation` parameter is used to move the camera around and
     ## refers to an object of `Transformation` type.
 
     new(result)
@@ -89,25 +92,26 @@ method fireRay*(c: PerspectiveCamera, u: float64, v: float64): Ray =
     ## is in the bottom left corner (following the diagram below)
     ## and the maximum value is 1 for both `u` and `v`.
     ## ::
-    ##                         
-    ##       (0,1)_____________________________(1,1) 
-    ##           |                             |      
-    ##         ^ |                             |      
-    ##         | |                             |      
-    ##         | |                             |      
-    ##       v | |                             |      
-    ##         | |                             |      
-    ##         | |                             |      
-    ##         | |_____________________________|      
-    ##       (0,0)  u --------------->          (0,1) 
-    ## 
+    ##
+    ##       (0,1)_____________________________(1,1)
+    ##           |                             |
+    ##         ^ |                             |
+    ##         | |                             |
+    ##         | |                             |
+    ##       v | |                             |
+    ##         | |                             |
+    ##         | |                             |
+    ##         | |_____________________________|
+    ##       (0,0)  u --------------->          (0,1)
+    ##
 
     # for PerspectiveCamera the origin of the ray is in the eye of the observer
-    result.origin = newPoint(-c.screenDistance, 0.0, 0.0) 
+    result.origin = newPoint(-c.screenDistance, 0.0, 0.0)
+    # The direction of the ray is given by
     result.dir = newVec(c.screenDistance, (1.0 - 2 * u) * c.aspectRatio, 2*v - 1)
     result.tmin = 1.0
-    return result.transform(c.transformation) 
-    # The result is the transformed ray, which corresponds to the transformed ray
+    return result.transform(c.transformation)
+    # The result is the transformed camera, which corresponds to the transformed ray
 
 method fireRay*(c: OrthogonalCamera, u: float64, v: float64): Ray =
 
@@ -117,22 +121,23 @@ method fireRay*(c: OrthogonalCamera, u: float64, v: float64): Ray =
     ## is in the bottom left corner (following the diagram below)
     ## and the maximum value is 1 for both `u` and `v`.
     ##
-    ##::                         
-    ##       (0,1)_____________________________(1,1) 
-    ##           |                             |      
-    ##         ^ |                             |      
-    ##         | |                             |      
-    ##         | |                             |      
-    ##       v | |                             |      
-    ##         | |                             |      
-    ##         | |                             |      
-    ##         | |_____________________________|      
-    ##       (0,0)  u --------------->          (0,1) 
-    ##     
+    ##::
+    ##       (0,1)_____________________________(1,1)
+    ##           |                             |
+    ##         ^ |                             |
+    ##         | |                             |
+    ##         | |                             |
+    ##       v | |                             |
+    ##         | |                             |
+    ##         | |                             |
+    ##         | |_____________________________|
+    ##       (0,0)  u --------------->          (0,1)
+    ##
 
     # for orthogonalCamera the origin of the ray is in in the point on the screen
     result.origin = newPoint(-1.0, (1.0 - 2 * u) * c.aspectRatio, 2 * v - 1)
+    # The direction of the ray is orthogonal to the screen
     result.dir = vecX
     result.tmin = 1.0
     return result.transform(c.transformation)
-    # The result is the transformed ray, which corresponds to the transformed ray
+    # The result is the transformed camera, which corresponds to the transformed ray
--- src/traynim/ray.nim
+++ src/traynim/ray.nim
@@ -46,15 +46,28 @@ proc newRay*(origin: Point, dir: Vec, tmin = 1e-5, tmax = Inf, depth = 0): Ray =
     result.depth = depth
 
 proc areClose*(a, b: Ray, epsilon = 1e-5): bool =
+
+    ## Determines if two rays are equal (to use with floating points)
+
     result = (a.origin.areClose(b.origin, epsilon)) and (
             a.dir.areClose(b.dir, epsilon))
 
 proc at*(ray: Ray, t: float64): Point =
+
+    ## Computes the point along the ray's path at some distance from the origin.
+    ## Returns a ``Point`` object representing the point in 3D space whose distance from the
+    ## ray's origin is equal to `t`, measured in units of the length of `dir` field.
+
     result = ray.origin + ray.dir * t
 
-proc transform*(ray: Ray, transformation: Transformation): Ray=
-        result.origin=transformation * ray.origin
-        result.dir=transformation * ray.dir
-        result.tmin=ray.tmin
-        result.tmax=ray.tmax
-        result.depth=ray.depth
+proc transform*(ray: Ray, transformation: Transformation): Ray =
+
+    ## Transforms a ray.
+    ## This procedure returns a new ray whose origin and direction are
+    ## the transformation of the original ray.
+
+    result.origin = transformation * ray.origin
+    result.dir = transformation * ray.dir
+    result.tmin = ray.tmin
+    result.tmax = ray.tmax
+    result.depth = ray.depth
