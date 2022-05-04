51c0f330aa3d8fb8f33965e7d9952d50e15d789a
Jacopo Fera <jacopof@gmail.com>
2022-05-04 21:10:57 +0200

    Add two spheres in demo to test orientation

--- src/traynim.nim
+++ src/traynim.nim
@@ -17,72 +17,96 @@
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import strutils, streams, hdrimages, cligen, world, cameras, imageTracer, shapes, transformations,
+import strutils, streams, hdrimages, cligen, world, cameras, imageTracer,
+        shapes, transformations,
     geometry, colors, options, sugar
 
-proc pfm2format(inPfmFileName: string, factor = 0.2, gamma = 1.0, outputFileName: string)=
+proc pfm2format(inPfmFileName: string, factor = 0.2, gamma = 1.0,
+        outputFileName: string) =
 
-    let inPfm = newFileStream( inPfmFileName, fmRead)
+    let inPfm = newFileStream(inPfmFileName, fmRead)
     var img = readPfmImage(inPfm)
     inPfm.close()
 
     echo ("File " & inPfmFileName & " has been read from disk")
 
-    img.normalizeImage( factor)
+    img.normalizeImage(factor)
     img.clampImage()
 
-    img.writeLdrImage( outputFileName,  gamma)
+    img.writeLdrImage(outputFileName, gamma)
 
-    echo ("File " &  outputFileName & " has been written to disk")
+    echo ("File " & outputFileName & " has been written to disk")
 
 
-proc demo(angleDeg = 0.0, orthogonal=false, width =640, height=480)=
+proc demo(angleDeg = 0.0, orthogonal = false, width = 640, height = 480) =
 
-    var image= newHDRImage(width,height)
+    var image = newHDRImage(width, height)
 
-    var world=newWorld()
+    var world = newWorld()
 
-    for x in [-0.5,0.5]:
-        for y in [-0.5,0.5]:
-            for z in [-0.5,0.5]:
+    for x in [-0.5, 0.5]:
+        for y in [-0.5, 0.5]:
+            for z in [-0.5, 0.5]:
                 world.addShape(
                     newSphere(
-                        transformation=translation(newVec(x, y, z))*scaling(newVec(0.1, 0.1, 0.1))
+                        transformation = translation(newVec(x, y, z))*scaling(
+                                newVec(0.1, 0.1, 0.1))
                     )
                 )
 
+    # Place two other balls in the cube, in order to check whether
+    # there are issues with the orientation of the image
+    
+    # First sphere at bottom 
+    world.addShape(
+        newSphere(
+            transformation = translation(newVec(0.0, 0.0, -0.5))*
+                scaling(newVec(0.1, 0.1, 0.1))
+            )
+        )
+
+    # Second sphere on the left face
+    world.addShape(
+        newSphere(
+            transformation = translation(newVec(0.0, 0.5, 0.0))*
+                scaling(newVec(0.1, 0.1, 0.1))
+            )
+        )
 
     let cameraTr = rotationZ(angleDeg) * translation(newVec(-1.0, 0.0, 0.0))
 
     var camera: Camera
 
     if orthogonal:
-        camera = newOrthogonalCamera(aspectRatio=width / height, transformation=cameraTr)
+        camera = newOrthogonalCamera(aspectRatio = width / height,
+                transformation = cameraTr)
     else:
-        camera = newPerspectiveCamera(aspectRatio=width / height, transformation=cameraTr)
+        camera = newPerspectiveCamera(aspectRatio = width / height,
+                transformation = cameraTr)
 
-    var tracer= newImageTracer(image,camera)
+    var tracer = newImageTracer(image, camera)
 
-    tracer.fireAllRays(ray => (if world.rayIntersection(ray).isSome: white else: black))
+    tracer.fireAllRays(ray => (if world.rayIntersection(
+            ray).isSome: white else: black))
 
-    let outPfm = newFileStream( "demo.pfm", fmWrite)
+    let outPfm = newFileStream("demo.pfm", fmWrite)
     tracer.image.writePfmImage(outPfm)
     echo "HDR demo image written to demo.pfm"
     outPfm.close()
 
     # Apply tone-mapping to the image
-    tracer.image.normalizeImage(factor=1.0)
+    tracer.image.normalizeImage(factor = 1.0)
     tracer.image.clampImage()
 
     # Save the LDR image
     tracer.image.writeLdrImage("demo.png")
     echo "PNG demo image written to demo.png"
 
-    
+
 
 
 
 
 when isMainModule:
 
-    dispatchMulti([pfm2format],[demo])
+    dispatchMulti([pfm2format], [demo])
