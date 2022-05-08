d92f6482d73b9a7c9366a530d492dd93885ecbcf
ottyanna <ottyanna@gmail.com>
2022-05-08 19:51:33 +0200

    [skip ci]: Added paramethers description in help

--- src/traynim.nim
+++ src/traynim.nim
@@ -20,86 +20,95 @@
 import strutils, streams, cligen, sugar, options
 
 import
-    cameras, 
-    colors, 
+    cameras,
+    colors,
     geometry,
-    hdrimages, 
+    hdrimages,
     imageTracer,
     shapes,
     transformations,
     world
 
-proc pfm2format(inPfmFileName: string, factor = 0.2, gamma = 1.0, outputFileName: string)=
+proc pfm2format(inPfmFileName: string, factor = 0.2, gamma = 1.0,
+        outputFileName: string) =
 
-    let inPfm = newFileStream( inPfmFileName, fmRead)
+    let inPfm = newFileStream(inPfmFileName, fmRead)
     var img = readPfmImage(inPfm)
     inPfm.close()
 
     echo ("File " & inPfmFileName & " has been read from disk")
 
-    img.normalizeImage( factor)
+    img.normalizeImage(factor)
     img.clampImage()
 
-    img.writeLdrImage( outputFileName,  gamma)
+    img.writeLdrImage(outputFileName, gamma)
 
-    echo ("File " &  outputFileName & " has been written to disk")
+    echo ("File " & outputFileName & " has been written to disk")
 
 
-proc demo(angleDeg = 0.0, orthogonal=false, width =640, height=480, fileName= "demo", format = "png")=
+proc demo(angleDeg = 0.0, orthogonal = false, width = 640, height = 480,
+        fileName = "demo", format = "png") =
 
-    var image= newHDRImage(width,height)
+    var image = newHDRImage(width, height)
 
-    var world=newWorld()
+    var world = newWorld()
 
-    for x in [-0.5,0.5]:
-        for y in [-0.5,0.5]:
-            for z in [-0.5,0.5]:
+    # Add spheres as vertices of a 0.5 side cube
+    for x in [-0.5, 0.5]:
+        for y in [-0.5, 0.5]:
+            for z in [-0.5, 0.5]:
                 world.addShape(
                     newSphere(
-                        transformation=translation(newVec(x, y, z))*scaling(newVec(0.1, 0.1, 0.1))
+                        transformation = translation(newVec(x, y, z))*scaling(
+                                newVec(0.1, 0.1, 0.1))
                     )
                 )
 
     # Place two other spheres in the cube, in order to check whether
     # there are issues with the orientation of the image
-    
-    # First sphere at bottom 
+
+    # First sphere at bottom
     world.addShape(
         newSphere(
-            transformation = translation(newVec(0.0, 0.0, -0.5))*
+            transformation = translation(newVec(0.0, 0.0, -0.5)) *
                 scaling(newVec(0.1, 0.1, 0.1))
-            )
         )
+    )
 
     # Second sphere on the left face
     world.addShape(
         newSphere(
-            transformation = translation(newVec(0.0, 0.5, 0.0))*
+            transformation = translation(newVec(0.0, 0.5, 0.0)) *
                 scaling(newVec(0.1, 0.1, 0.1))
-            )
         )
+    )
 
-
+    # DEfine transformation on camera
     let cameraTr = rotationZ(angleDeg) * translation(newVec(-1.0, 0.0, 0.0))
 
     var camera: Camera
 
     if orthogonal:
-        camera = newOrthogonalCamera(aspectRatio=width / height, transformation=cameraTr)
+        camera = newOrthogonalCamera(aspectRatio = width / height,
+                transformation = cameraTr)
     else:
-        camera = newPerspectiveCamera(aspectRatio=width / height, transformation=cameraTr)
+        camera = newPerspectiveCamera(aspectRatio = width / height,
+                transformation = cameraTr)
 
-    var tracer= newImageTracer(image,camera)
+    var tracer = newImageTracer(image, camera)
 
-    tracer.fireAllRays(ray => (if world.rayIntersection(ray).isSome: white else: black))
+    # Run ray tracer with "on/off" renderer
+    tracer.fireAllRays(ray => (if world.rayIntersection(
+            ray).isSome: white else: black))
 
-    let outPfm = newFileStream( fileName & ".pfm", fmWrite)
+    # Save the HDR image
+    let outPfm = newFileStream(fileName & ".pfm", fmWrite)
     tracer.image.writePfmImage(outPfm)
     echo "HDR demo image written to " & fileName & ".pfm"
     outPfm.close()
 
     # Apply tone-mapping to the image
-    tracer.image.normalizeImage(factor=1.0)
+    tracer.image.normalizeImage(factor = 1.0)
     tracer.image.clampImage()
 
     # Save the LDR image
@@ -123,4 +132,18 @@ when isMainModule:
 
     echo traynim
 
-    dispatchMulti([pfm2format, help={"outputFileName": "Formats allowed are PNG, PPM, BMP and QOI"}],[demo])
+    dispatchMulti(
+        [pfm2format, 
+            help = {"outputFileName":" Path to output file (PNG, PPM, BMP or QOI formats)", 
+                    "factor":"Multiplicative factor",
+                    "gamma":"Exponent for gamma-correction",
+                    "inPfmFileName":"Path to input file (PFM)" }
+        ],
+        [demo,
+            help = {"angleDeg": "Angle rotation of the camera (Degrees)",
+                    "orthogonal" : "Perspective or orthogonal camera (DefaultPerspective)",
+                    "width" : "Width of the image",
+                    "height" : "Height of the image",
+                    "fileName" : "Path to output file without format",
+                    "format": "PNG, PPM, BMP or QOI formats"}
+        ])
