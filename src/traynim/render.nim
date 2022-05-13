c6d2b3b27d0d83b867d520b2799f4d545176fc0c
ottyanna <ottyanna@gmail.com>
2022-05-13 18:41:57 +0200

    [skip ci]: Added the two renderers, call for FlatRenderer still commented

--- src/traynim/render.nim
+++ src/traynim/render.nim
@@ -16,47 +16,57 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
+## This module implements different algorithms for rendering
+
 import colors, ray, world
 import options
 
-type Renderer = ref object of RootObj
-        world: World
-        backgroundColor : Color
+type Renderer* = ref object of RootObj
+        ## generic renderer
+        world*: World
+        backgroundColor* : Color
 
-method call(renderer : Renderer, ray :Ray): Color {.base.}=
+method newBaseRenderer*(renderer: Renderer,world: World, backgroundColor = black) {.base.}=
+    renderer.backgroundColor=backgroundColor
+    renderer.world = world
+    
+method call*(renderer : Renderer, ray :Ray): Color {.base.}=
     quit "to override!"
 
-type OnOffRenderer = ref object of Renderer
-    ## A on/off renderer.
-    color: Color
+type OnOffRenderer* = ref object of Renderer
+    ## A on/off renderer: everything is of two colors (`backgroundColor` and `color`). 
+    color*: Color
 
 proc newOnOffRenderer*(world: World, backgroundColor = black, color = white): OnOffRenderer=
     new(result)
-    result.backgroundColor=backgroundColor
-    result.world = world
+    result.newBaseRenderer(world, backgroundColor)
     result.color = color
 
 method call*(renderer: OnOffRenderer, ray: Ray) : Color=
-
+    ## Calls the image rendering where the shapes are in solid color `color`.
     if renderer.world.rayIntersection(ray).isSome:
         return renderer.color 
     else: return renderer.backgroundColor
 
-#[ 
-class FlatRenderer(Renderer):
-    """A «flat» renderer
-    This renderer estimates the solution of the rendering equation by neglecting any contribution of the light.
-    It just uses the pigment of each surface to determine how to compute the final radiance."""
 
-    def __init__(self, world: World, background_color: Color = BLACK):
-        super().__init__(world, background_color)
+type FlatRenderer* = ref object of Renderer
+    ## A «flat» renderer
+    ## This renderer estimates the solution of the rendering equation by neglecting any contribution of the light.
+    ## It just uses the pigment of each surface to determine how to compute the final radiance.
 
-    def __call__(self, ray: Ray) -> Color:
-        hit = self.world.ray_intersection(ray)
-        if not hit:
-            return self.background_color
+proc newFlatRenderer*(world: World, backgroundColor = black) : FlatRenderer =
+    new(result)
+    result.newBaseRenderer(world,backgroundColor)
 
-        material = hit.shape.material
+    
+#[
+method call*(renderer: FlatRenderer, ray: Ray) : Color=
+        let hit = renderer.world.rayIntersection(ray)
+        if hit.isNone:
+            return renderer.backgroundColor
 
-        return (material.brdf.pigment.get_color(hit.surface_point) +
-                material.emitted_radiance.get_color(hit.surface_point)) ]#
+        let material = (hit.get).shape.material
+
+        return (material.brdf.pigment.getColor((hit.get).surfacePoint) +
+                material.emittedRadiance.getColor((hit.get).surfacePoint))]#
